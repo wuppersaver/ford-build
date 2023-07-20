aacb306532d30a59be3edf0bea9ca0b8ad4163e1
Felix Mildner <felix@wuppersaver.de>
2023-07-20 01:31:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,4 @@ set -e
 ROOT=$(find . -name $1 -exec dirname {} \;)
 echo "FORD project file located in ${ROOT}"
 
-# Get output directory, where documentation is written
-# This can later be used for publishing
-OUTPUT_DIR=$(grep 'output_dir' ${ROOT}/$1 | sed 's/output_dir: \(.*\)/\1/')
-OUTPUT_DIR="${ROOT}/${OUTPUT_DIR}"
-echo "Documentation written to ${OUTPUT_DIR}"
-# Set output directory variable
-echo ::set-output name=output_dir::$OUTPUT_DIR
-
-# Build documentation
-echo "Building documentation"
-ford ${ROOT}/$1
+pwd
